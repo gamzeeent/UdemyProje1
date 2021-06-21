0874f89da9b08082dff7c03905f17bbe1e69b0ca
gamzeeent <80635990+gamzeeent@users.noreply.github.com>
2021-06-21 23:57:08 +0300

    .
    startfloor yapıldı.

---
+++ proje1/Assets/GameFolders/Scripts/Concretes/Controllers/StartFloorController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace proje1.Controllers
+{
+public class StartFloorController : MonoBehaviour
+{
+        private void OnCollisionExit(Collision other)
+        {
+            PlayerController player = other.collider.GetComponent<PlayerController>();
+            if(player != null)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
